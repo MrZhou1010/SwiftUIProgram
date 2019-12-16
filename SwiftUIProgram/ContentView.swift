6f4f1c303ff9588a0962b9ed3373b776129b9ba3
MrZhou1010 <272587392@qq.com>
2019-12-16 09:35:10 +0800

    create project

---
+++ SwiftUIProgram/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIProgram
+//
+//  Created by Mr.Z on 2019/12/13.
+//  Copyright © 2019 Mr.Z. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
